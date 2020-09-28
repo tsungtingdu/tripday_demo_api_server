0c5c751587583a27e8438c71cc2a930dd3627c3e
tsungtingdu <tsungtingdu@gmail.com>
2020-09-28 18:46:12 +0800

    project init

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+const fs = require("fs");
+const path = require("path");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+server.use(router);
+
+server.listen(3001, () => {
+  console.log("JSON Server is running");
+});
